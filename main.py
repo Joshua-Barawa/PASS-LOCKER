9717208e6e6c295e8ccf6403bf05ab260aab4900
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-01-22 19:05:48 +0300

    added login and logout functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from passwords import password
 
 
 class Main:
-    print("Welcome to password locker, Enter number to choose option")
+    print("Welcome to password locker, Enter option number to continue")
     # print("1 LOG IN\n2 SIGN UP\n3 EXIT")
 
     while True:
@@ -14,10 +14,11 @@ class Main:
             username = input("Enter username: ")
             pass_word = input("Enter password: ")
 
-            new_account = account.Account(username, pass_word)
-            if not new_account.login(username):
+            login = account.login(username)
+            if not login:
                 print("Invalid user credentials")
             else:
+                print("Welcome back ", username, "Enter option number to continue")
                 while True:
                     print("1 Create new password\n2 View saved passwords\n3 Delete password\n4 Logout")
                     choice = int(input())
@@ -52,7 +53,7 @@ class Main:
             account.create_account()
 
             for user in account.user_accounts:
-                print("Account created with username and password ", user.username + "/"+ user.password)
+                print("Account created successfully")
                 while True:
                     print("1 Create new password\n2 View saved passwords\n3 Delete password\n4 Logout")
                     choice = int(input())
